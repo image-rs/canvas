0e6288b6054f2b0707334ebfced74878bc080912
Andreas Molzer <andreas.molzer@gmx.de>
2019-04-20 19:27:01 +0200

    Add a reinterpretation cast test

--- src/buf.rs
+++ src/buf.rs
@@ -79,12 +79,30 @@ fn prefix_slice<B, T>(slice: B) -> (B, B) where B: ByteSlice
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::pixels::MAX;
+    use crate::pixels::{U8, U16, U32, MAX};
 
     #[test]
     fn single_max_element() {
         let mut buf = Buf::new(mem::size_of::<MaxAligned>());
         let slice = buf.as_mut_pixels(MAX);
         assert!(slice.len() == 1);
+    }
+
+    #[test]
+    fn reinterpret() {
+        let mut buf = Buf::new(mem::size_of::<u32>());
+        assert!(buf.as_mut_pixels(U32).len() >= 1);
+        buf.as_mut_pixels(U16).iter_mut().for_each(
+            |p| *p = 0x0f0f);
+        buf.as_pixels(U32).iter().for_each(
+            |p| assert_eq!(*p, 0x0f0f0f0f));
+        buf.as_pixels(U8).iter().for_each(
+            |p| assert_eq!(*p, 0x0f));
+
+        buf.as_mut_pixels(U8)
+            .iter_mut()
+            .enumerate()
+            .for_each(|(idx, p)| *p = idx as u8);
+        assert_eq!(u32::from_be(buf.as_pixels(U32)[0]), 0x00010203);
     }
 }
