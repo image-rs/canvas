196c03d13f3068c0bdad843765001b277f58aa44
HeroicKatora <andreas.molzer@gmx.de>
2019-04-29 15:02:36 +0200

    Propagate resizing to the exposed Rec buffer
    This duplicates the methods to work both on the logical element count
    and the byte length. Except shrink_to_fit which instead recalculates the
    exact byte length based on the current element type.

--- src/rec.rs
+++ src/rec.rs
@@ -27,9 +27,7 @@ impl<P: AsBytes + FromBytes> Rec<P> {
     /// Provides the opportunity to construct the pixel argument via other means than the trait,
     /// for example a dynamically checked expression.
     pub fn new_for_pixel(pixel: Pixel<P>, count: usize) -> Self {
-        let mem_size = pixel.size().checked_mul(count).unwrap_or_else(
-            || panic!("Requested count overflows memory size"));
-        Self::bytes_for_pixel(pixel, mem_size)
+        Self::bytes_for_pixel(pixel, mem_size(pixel, count))
     }
 
     /// Allocate a pixel buffer by providing the byte count you wish to allocate.
@@ -38,6 +36,46 @@ impl<P: AsBytes + FromBytes> Rec<P> {
             inner: Buffer::new(mem_size),
             pixel,
         }
+    }
+
+    /// Change the number of pixels.
+    ///
+    /// This will always reallocate the buffer if the size exceeds the current capacity.
+    pub fn resize(&mut self, count: usize) {
+        self.resize_bytes(mem_size(self.pixel, count))
+    }
+
+    /// Change the size in bytes.
+    ///
+    /// The length is afterwards equal to `bytes / mem::size_of::<P>()`, i.e. the quotient rounded
+    /// down.
+    ///
+    /// This will always reallocate the buffer if the size exceeds the current capacity.
+    pub fn resize_bytes(&mut self, bytes: usize) {
+        self.inner.resize(bytes)
+    }
+
+    /// Reallocate the slice to contain exactly as many bytes as necessary.
+    ///
+    /// The number of contained elements is not changed. However, the number of elements
+    /// interpreted as a different type may change.
+    ///
+    /// ```
+    /// # use canvas::Rec;
+    /// let rec_u8 = Rec::<u8>::new(7);
+    /// assert_eq!(rec_u8.len(), 7);
+    ///
+    /// let mut rec_u32 = rec_u8.reinterpret::<u32>();
+    /// assert_eq!(rec_u32.len(), 1);
+    /// rec_u32.shrink_to_fit();
+    ///
+    /// let rec_u8 = rec_u32.reinterpret::<u8>();
+    /// assert_eq!(rec_u8.len(), 4);
+    /// ```
+    pub fn shrink_to_fit(&mut self) {
+        let exact_size = mem_size(self.pixel, self.len());
+        self.resize_bytes(exact_size);
+        self.inner.shrink_to_fit();
     }
 
     pub fn as_slice(&self) -> &[P] {
@@ -51,6 +89,11 @@ impl<P: AsBytes + FromBytes> Rec<P> {
     /// The number of accessible elements for the current type.
     pub fn len(&self) -> usize {
         self.as_slice().len()
+    }
+
+    /// The number of elements that can fit without reallocation.
+    pub fn capacity(&self) -> usize {
+        self.inner.capacity() / self.pixel.size()
     }
 
     pub fn as_bytes(&self) -> &[u8] {
@@ -67,6 +110,11 @@ impl<P: AsBytes + FromBytes> Rec<P> {
     /// capacity of the storage.
     pub fn byte_len(&self) -> usize {
         self.as_bytes().len()
+    }
+
+    /// The total number of managable bytes.
+    pub fn byte_capacity(&self) -> usize {
+        self.inner.capacity()
     }
 
     /// Reinterpret the buffer for a different type of pixel.
@@ -92,6 +140,11 @@ impl<P: AsBytes + FromBytes> Rec<P> {
             pixel,
         }
     }
+}
+
+fn mem_size<P>(pixel: Pixel<P>, count: usize) -> usize {
+    pixel.size().checked_mul(count).unwrap_or_else(
+        || panic!("Requested count overflows memory size"))
 }
 
 impl<P: AsBytes + FromBytes> Deref for Rec<P> {
