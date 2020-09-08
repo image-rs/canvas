09104361c109626104ca283e5b262c239d9c8f0a
Andreas Molzer <andreas.molzer@gmx.de>
2020-09-08 20:43:02 +0200

    Relax Pod requirements for Pixel

--- src/pixel.rs
+++ src/pixel.rs
@@ -3,19 +3,35 @@
 // Copyright (c) 2019, 2020 The `image-rs` developers
 use core::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
 use core::marker::PhantomData;
-use core::{fmt, hash, mem};
+use core::{fmt, hash, mem, ptr, slice};
+
+use crate::buf::buf;
 
 /// Marker struct to denote a pixel type.
 ///
 /// Can be constructed only for types that have expected alignment and no byte invariants. It
 /// always implements `Copy` and `Clone`, regardless of the underlying type and is zero-sized.
+///
+/// This is the central encapsulation of unsafety in this crate. It utilizes `bytemuck` for a safe
+/// interface but permits other types with an unsafe interface, and offers the cast operations
+/// without a bound on the `Pod` trait. Note that `Pod` is a pure marker trait; its properties must
+/// hold even if it is not explicitly mentioned. If all constructors (safely or unsafely) ensure
+/// that its properties hold we can use `Pixel` as a witness type for the bound and subsequently
+/// write interfaces to take an instance instead of having a static type bound. This achieves two
+/// effects:
+/// * Firstly, it makes the interface independent of the chosen transmutation crate. Potentially we
+///   will have a method to construct the `Pixel` via a `core` trait.
+/// * Secondly, it allows creating pixels of third-party types for which the bound can not be
+///   implemented. Crucially, this includes SIMD representations that would be a burden to support
+///   directly. And conversely you can also deal with arbitrary existing pixels without a bound in
+///   your own interfaces!
 pub struct Pixel<P: ?Sized>(PhantomData<P>);
 
-/// Describes a type which can represent a `Pixel`.
+/// Describes a type which can represent a `Pixel` and for which this is statically known.
 pub trait AsPixel {
     /// Get the pixel struct for this type.
     ///
-    /// The naive implementation of merely unwraping the result of `Pixel::for_type` **panics** on
+    /// The naive implementation of merely unwrapping the result of `Pixel::for_type` **panics** on
     /// any invalid type. This trait should only be implemented when you know for sure that the
     /// type is correct.
     fn pixel() -> Pixel<Self>;
@@ -25,7 +41,7 @@ pub(crate) const MAX_ALIGN: usize = 16;
 
 /// A byte-like-type that is aligned to the required max alignment.
 ///
-/// This type does not contain padding and implements `FromBytes`.
+/// This type does not contain padding and implements `Pod`.
 #[derive(Clone, Copy)]
 #[repr(align(16))]
 #[repr(C)]
@@ -81,6 +97,20 @@ impl<P: bytemuck::Pod> Pixel<P> {
 }
 
 impl<P> Pixel<P> {
+    /// Create a witness certifying `P` as a pixel without checks.
+    ///
+    /// # Safety
+    ///
+    /// The type `P` must not:
+    /// * have any validity invariants, i.e. is mustn't contain any padding.
+    /// * have any safety invariants. This implies it can be copied.
+    /// * have an alignment larger than [`MaxAligned`].
+    ///
+    /// [`MaxAligned`]: struct.MaxAligned.html
+    pub unsafe fn new_unchecked() -> Self {
+        Pixel(PhantomData)
+    }
+
     /// Proxy of `core::mem::align_of`.
     pub fn align(self) -> usize {
         mem::align_of::<P>()
@@ -89,6 +119,73 @@ impl<P> Pixel<P> {
     /// Proxy of `core::mem::size_of`.
     pub fn size(self) -> usize {
         mem::size_of::<P>()
+    }
+
+    /// Copy a pixel.
+    ///
+    /// Note that this does not require `Copy` because that requirement was part of the
+    /// requirements of constructing this `Pixel` witness.
+    pub fn copy_val(self, val: &P) -> P {
+        // SAFETY: by the constructor, this type can be copied byte-by-byte.
+        unsafe { ptr::read(val) }
+    }
+
+    pub(crate) fn cast_buf<'buf>(self, buffer: &'buf buf) -> &'buf [P] {
+        debug_assert_eq!(buffer.as_ptr() as usize % mem::align_of::<MaxAligned>(), 0);
+        debug_assert_eq!(buffer.as_ptr() as usize % mem::align_of::<P>(), 0);
+        // Safety:
+        // * data is valid for reads as memory size is not enlarged
+        // * lifetime is not changed
+        // * validity for arbitrary data as required by Pixel constructor
+        // * alignment checked by Pixel constructor
+        // * the size fits in an allocation, see first bullet point.
+        unsafe {
+            if mem::size_of::<P>() == 0 {
+                slice::from_raw_parts(buffer.as_ptr() as *const P, usize::MAX)
+            } else {
+                slice::from_raw_parts(
+                    buffer.as_ptr() as *const P,
+                    buffer.len() / mem::size_of::<P>(),
+                )
+            }
+        }
+    }
+
+    pub(crate) fn cast_mut_buf<'buf>(self, buffer: &'buf mut buf) -> &'buf mut [P] {
+        debug_assert_eq!(buffer.as_ptr() as usize % mem::align_of::<MaxAligned>(), 0);
+        debug_assert_eq!(buffer.as_ptr() as usize % mem::align_of::<P>(), 0);
+        // Safety:
+        // * data is valid for reads and writes as memory size is not enlarged
+        // * lifetime is not changed
+        // * validity for arbitrary data as required by Pixel constructor
+        // * alignment checked by Pixel constructor
+        // * the size fits in an allocation, see first bullet point.
+        unsafe {
+            if mem::size_of::<P>() == 0 {
+                slice::from_raw_parts_mut(buffer.as_mut_ptr() as *mut P, usize::MAX)
+            } else {
+                slice::from_raw_parts_mut(
+                    buffer.as_mut_ptr() as *mut P,
+                    buffer.len() / mem::size_of::<P>(),
+                )
+            }
+        }
+    }
+
+    pub(crate) fn cast_bytes<'buf>(self, pixel: &'buf [P]) -> &'buf [u8] {
+        // Safety:
+        // * lifetime is not changed
+        // * keeps the exact same size
+        // * validity for byte reading checked by Pixel constructor
+        unsafe { slice::from_raw_parts(pixel.as_ptr() as *const u8, mem::size_of_val(pixel)) }
+    }
+
+    pub(crate) fn cast_mut_bytes<'buf>(self, pixel: &'buf mut [P]) -> &'buf mut [u8] {
+        // Safety:
+        // * lifetime is not changed
+        // * keeps the exact same size
+        // * validity as bytes checked by Pixel constructor
+        unsafe { slice::from_raw_parts_mut(pixel.as_ptr() as *mut u8, mem::size_of_val(pixel)) }
     }
 }
 
