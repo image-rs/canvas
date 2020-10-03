c659d47cd0620e443f5d3e674912ee7dc389cfb1
Andreas Molzer <andreas.molzer@gmx.de>
2020-10-03 02:22:43 +0200

    Progress drm with more docs, basic FormatInfo

--- src/drm.rs
+++ src/drm.rs
@@ -2,19 +2,30 @@ use crate::layout::DynLayout;
 
 /// A direct rendering manager format info.
 ///
+/// The format information describes the byte layout of pixels of that particular type.
+///
 /// This structure is a cleaned-up version of the one present in the Linux kernel rendering
 /// subsystem, with deprecated fields removed. It is not a `Layout` itself since it is not
 /// guaranteed to be internally consistent, it does not yet describe width and height or it might
 /// otherwise not be supported. Try to convert it to a `DynLayout`.
 ///
-/// See: the Linux kernel header `drm/drm_fourcc.h`.
+/// See: the Linux kernel header `drm/drm_fourcc.h` and
+///
+/// https://www.kernel.org/doc/html/latest/gpu/drm-kms.html#c.drm_format_info
 #[derive(Clone, Copy, Debug, Hash)]
 pub struct DrmFormatInfo {
     /// The 4CC format identifier.
     pub format: FourCC,
     /// The number of image color planes (1 to 3).
     pub num_planes: u8,
-    /// The number of bytes per block.
+    /// Number of bytes per block (per plane).
+    ///
+    /// Blocks are defined as a rectangle of pixels which are stored next to each other in a byte
+    /// aligned memory region. Since we have no backwards compatibility considerations there is no
+    /// `cpp` member.
+    ///
+    /// For formats that are intended to be used only with non-linear modifiers char_per_block must
+    /// be 0 in the generic format table.
     pub char_per_block: [u8; 4],
     /// The width of a block in pixels.
     pub block_w: [u8; 4],
@@ -32,7 +43,9 @@ pub struct DrmFormatInfo {
 
 /// A descriptor for a single frame buffer.
 ///
-/// In Linux, used to request new buffers or reallocation of buffers.
+/// In Linux, used to request new buffers or reallocation of buffers. Here, we use it similarly as
+/// the builder type to fallibly construct a `DrmFramebuffer`, a complete layout descriptor for one
+/// sized image.
 ///
 /// See: the Linux kernel header `drm/drm_mode.h`.
 #[derive(Clone, Copy, Debug, Hash)]
@@ -47,6 +60,8 @@ pub struct DrmFramebufferCmd {
 }
 
 /// The filled-in info about a frame buffer.
+///
+/// This is equivalent to `drm_framebuffer`, minus the kernel internal stuff.
 pub(crate) struct DrmFramebuffer {
     pub format: DrmFormatInfo,
     pub pitches: [u32; 4],
@@ -75,6 +90,20 @@ pub struct BadDrmError {
 pub struct FourCC(u32);
 
 impl DrmFormatInfo {
+    /// Default values for an info of a format that has 1×1 blocks, i.e. each pixel is stored
+    /// individually. Some info is left out to require it being filled in.
+    const PIXEL1_TEMPLATE: Self = DrmFormatInfo {
+        format: FourCC::INVALID,
+        num_planes: 0,
+        char_per_block: [0; 4],
+        block_w: [1; 4],
+        block_h: [1; 4],
+        hsub: 1,
+        vsub: 1,
+        has_alpha: false,
+        is_yuv: false,
+    };
+
     /// Create a layout with particular dimensions.
     ///
     /// This is a partial function to represent that not all descriptors can be convert to a
@@ -109,7 +138,7 @@ impl FourCC {
     /// 16 bpp xrgb with 4 bits each.
     pub const XRGB444: Self = FourCC::from(*b"XR12");
     /// 16 bpp xbgr with 4 bits each.
-    pub const XBRG444: Self = FourCC::from(*b"XB12");
+    pub const XBGR444: Self = FourCC::from(*b"XB12");
     /// 16 bpp rgbx with 4 bits each.
     pub const RGBX444: Self = FourCC::from(*b"RX12");
     /// 16 bpp bgrx with 4 bits each.
@@ -120,7 +149,28 @@ impl FourCC {
         FourCC(arr[0] as u32 | (arr[1] as u32) << 8 | (arr[2] as u32) << 16 | (arr[3] as u32) << 24)
     }
 
-    fn info(self) -> Result<DrmFormatInfo, BadDrmError> {
-        Err(BadDrmError { _private: () })
+    pub fn info(self) -> Result<DrmFormatInfo, BadDrmError> {
+        let mut info = match self {
+            FourCC::C8 => DrmFormatInfo {
+                num_planes: 1,
+                char_per_block: [1, 0, 0, 0],
+                ..DrmFormatInfo::PIXEL1_TEMPLATE
+            },
+            FourCC::RGB332 | FourCC::BGR332 => DrmFormatInfo {
+                num_planes: 1,
+                char_per_block: [1, 0, 0, 0],
+                ..DrmFormatInfo::PIXEL1_TEMPLATE
+            },
+            FourCC::XRGB444 | FourCC::XBGR444 | FourCC::RGBX444 | FourCC::BGRX444 => {
+                DrmFormatInfo {
+                    num_planes: 1,
+                    char_per_block: [2, 0, 0, 0],
+                    ..DrmFormatInfo::PIXEL1_TEMPLATE
+                }
+            }
+            _ => return Err(BadDrmError { _private: () }),
+        };
+        info.format = self;
+        Ok(info)
     }
 }
